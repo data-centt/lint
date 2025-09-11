15bc0bc8fa9003f9215e52f5557d678fa90e6259
Daniel15568 <1danielemakporuena@gmail.com>
2025-09-11 22:05:09 +0100

    imports

--- backend/upload_resume.py
+++ backend/upload_resume.py
@@ -1,1 +1,8 @@
-
+import os
+import uuid
+from typing import Optional, Dict
+from PyPDF2 import PdfReader
+from docx import Document as DocxDocument
+from firebase_admin import firestore
+from google.cloud.exceptions import GoogleCloudError
+from config import db, bucket
