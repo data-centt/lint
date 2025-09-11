78a163641a8a17dd01f7ccdfbf43a21c7fc8eff5
Daniel15568 <1danielemakporuena@gmail.com>
2025-09-11 22:11:30 +0100

    h

--- backend/upload_resume.py
+++ backend/upload_resume.py
@@ -6,3 +6,10 @@ from docx import Document as DocxDocument
 from firebase_admin import firestore
 from google.cloud.exceptions import GoogleCloudError
 from config import db, bucket
+
+
+"""
+This file handles the imports and outputs of the files and automatically pick the most recent file
+upload.
+"""
+
