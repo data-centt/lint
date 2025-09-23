929267068c760f837c83927e0f1e418d6927327b
Daniel15568 <1danielemakporuena@gmail.com>
2025-09-23 20:57:20 +0100

    updates on the importw

--- backend/main.py
+++ backend/main.py
@@ -7,3 +7,7 @@ from typing import Any, Dict, Iterable, List, Union
 
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import HTTPException
